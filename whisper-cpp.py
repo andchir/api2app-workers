edf8c28bee11f86cc0a4d9b7311861be8798e9c8
andrew <andrew@merlinsoftware.com>
2025-03-02 15:43:52 +0300

    whisper worker upd - duration limit

--- whisper-cpp.py
+++ whisper-cpp.py
@@ -5,13 +5,16 @@ import ffmpeg
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.queue_manager import polling_queue, upload_queue_files, send_queue_error, send_queue_result_dict
+from utils.upload_file import delete_old_files
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 WHISPER_CLI_PATH = '/home/andrew/PycharmProjects/whisper.cpp/build/bin/whisper-cli'
 WHISPER_MODEL_PATH = '/home/andrew/PycharmProjects/whisper.cpp/models/ggml-medium.bin'
+MAX_DURATION = 30 * 60 # 30 minutes maximum
 
 
 def processing(queue_item):
+    print()
     print('New task:', queue_item['uuid'])
     upload_dir_path = os.path.join(ROOT_DIR, 'uploads', 'whisper')
 
@@ -26,14 +29,16 @@ def processing(queue_item):
     # Options
     options = queue_item['data'] if 'data' in queue_item else dict()
     language = options['language'] if 'language' in options and type(options['language']) is str else 'en'
-    output_type = options['output_type'] if 'output_type' in options and type(options['output_type']) is str else 'txt'
+    output_type = options['output_type'] \
+        if 'output_type' in options \
+        and options['output_type'] in ['txt', 'csv', 'srt', 'json', 'json-full'] else 'txt'
 
     # Converting audio file to WAV format
     audio_wav_path = os.path.join(upload_dir_path, queue_item['uuid'] + '.wav')
     (
         ffmpeg
         .input(audio_file_path)
-        .output(audio_wav_path, ar=16000, ac=1, acodec='pcm_s16le')
+        .output(audio_wav_path, ar=16000, ac=1, acodec='pcm_s16le', t=MAX_DURATION)
         .run(capture_stdout=True, capture_stderr=True)
     )
 
@@ -55,11 +60,13 @@ def processing(queue_item):
     result = subprocess.run(command, capture_output=True, text=True)
 
     if 'saving output to' in str(result):
-        if output_type == 'txt':
-            output_file_path += '.txt'
+        if output_type in ['json', 'json-full']:
+            output_file_path += '.json'
+        else:
+            output_file_path += '.' + output_type
         if not os.path.isfile(output_file_path):
             print(f'Output file not found. Send error message - Processing error.')
-            print('out_file_path:', output_file_path)
+            print()
             send_queue_error(queue_item['uuid'], 'Processing error. Please try again later.')
             return
 
@@ -73,6 +80,11 @@ def processing(queue_item):
 
     else:
         send_queue_error(queue_item['uuid'], 'Processing error. Please try again later.')
+
+    # Delete old files
+    deleted_input = delete_old_files(upload_dir_path, max_hours=2)
+    print('Deleted old files: ', deleted_input)
+    print()
 
 
 if __name__ == '__main__':
